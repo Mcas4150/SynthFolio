a8696cbb1ffe3a85034806690fceee34b59346e3
Michael Cassidy <mikecassidy0000@gmail.com>
2017-12-05 14:52:56 -0500

    synthesizer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
-root to: 'pages#home'
+  get 'pages/newhome'
+root to: 'pages#newhome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
