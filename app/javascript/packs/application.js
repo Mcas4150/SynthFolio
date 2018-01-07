931e665852afdc8be93e8b038ed5a712c7e06624
Michael Cassidy <mikecassidy0000@gmail.com>
2018-01-07 14:18:01 -0500

    ios

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -623,7 +623,27 @@ document.addEventListener("touchstart", play);
                             }
                         }
 
+  var isUnlocked = false;
+function unlock() {
 
+  if(isIOS || this.unlocked)
+    return;
+
+  // create empty buffer and play it
+  var buffer = myContext.createBuffer(1, 1, 22050);
+  var source = myContext.createBufferSource();
+  source.buffer = buffer;
+  source.connect(myContext.destination);
+  source.noteOn(0);
+
+  // by checking the play state after some time, we know if we're really unlocked
+  setTimeout(function() {
+    if((source.playbackState === source.PLAYING_STATE || source.playbackState === source.FINISHED_STATE)) {
+      isUnlocked = true;
+    }
+  }, 0);
+
+}
 
 
 });
